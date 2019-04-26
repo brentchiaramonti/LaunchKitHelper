60b426f3fb5778af72600cf0adbc65167eaf5384
brentchiaramonti <35432244+brentchiaramonti@users.noreply.github.com>
2019-04-25 21:57:25 -0500

    Update linkMaker.js

--- linkMaker.js
+++ linkMaker.js
@@ -16,17 +16,11 @@ function populateWebpage(xml){
 
 	var parser = new DOMParser();
     xmlDoc = parser.parseFromString(xml.responseText, "text/xml");
-	console.log(xmlFormated);
-	console.log(xmlDoc);
 	txt = "";
 	title = xmlDoc.getElementsByTagName("item")[0].childNodes[1].childNodes[0].nodeValue;
-	console.log(title)
 	name = title + "Links"
 	items = xmlDoc.getElementsByTagName("item");
 	for(i = 0; i < items.length; i++){
-		console.log(i)
-		console.log(items)
-		console.log(items[i])
 		txt += "<label><input type=\"checkbox\" name=\"" + name + "\">" + items[i].childNodes[1].firstChild.nodeValue + "</label><br>";
 	}
 	document.getElementById("checkboxes").innerHTML = txt;
